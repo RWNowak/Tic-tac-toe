db67706ea85ca1d26eae54ec035b2c5c8d0ba823
Rafał Nowak <rwojciechnowak@gmail.com>
2022-05-13 11:38:23 +0200

    Dodaj pliki projektów.

---
+++ KółkoKrzyżyk/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace KółkoKrzyżyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.a1 = new System.Windows.Forms.Button();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(70, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kółko i Krzyżyk";
+            // 
+            // a1
+            // 
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.a1.Location = new System.Drawing.Point(12, 77);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(102, 80);
+            this.a1.TabIndex = 1;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.button_click);
+            // 
+            // a2
+            // 
+            this.a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.a2.Location = new System.Drawing.Point(120, 77);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(102, 80);
+            this.a2.TabIndex = 2;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.button_click);
+            // 
+            // a3
+            // 
+            this.a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.a3.Location = new System.Drawing.Point(228, 77);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(102, 80);
+            this.a3.TabIndex = 3;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.button_click);
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b1.Location = new System.Drawing.Point(12, 163);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(102, 80);
+            this.b1.TabIndex = 4;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button_click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b2.Location = new System.Drawing.Point(120, 163);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(102, 80);
+            this.b2.TabIndex = 5;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.button_click);
+            // 
+            // b3
+            // 
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.b3.Location = new System.Drawing.Point(228, 163);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(102, 80);
+            this.b3.TabIndex = 6;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.button_click);
+            // 
+            // c1
+            // 
+            this.c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.c1.Location = new System.Drawing.Point(12, 249);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(102, 80);
+            this.c1.TabIndex = 7;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.button_click);
+            // 
+            // c2
+            // 
+            this.c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.c2.Location = new System.Drawing.Point(120, 249);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(102, 80);
+            this.c2.TabIndex = 8;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.button_click);
+            // 
+            // c3
+            // 
+            this.c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.c3.Location = new System.Drawing.Point(228, 249);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(102, 80);
+            this.c3.TabIndex = 9;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.button_click);
+            // 
+            // button10
+            // 
+            this.button10.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button10.Location = new System.Drawing.Point(63, 371);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(226, 41);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Wyczyść";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 450);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ KółkoKrzyżyk/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KółkoKrzyżyk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool ruch = false;
+        int ruchIlosc = 0;
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button przycisk = (Button)sender;
+            if (ruch == true)
+            {
+                przycisk.Text = "X";
+            }
+            else
+            {
+                przycisk.Text = "O";
+            }
+            ruch = !ruch;
+            ruchIlosc++;
+            przycisk.Enabled = false;
+
+            sprawdz();
+        }
+        private void sprawdz()
+        {
+            bool znalezionoZwyciezce = false;
+            if ((a1.Text == a2.Text) && (a2.Text == a3.Text) && a1.Text != "") //rząd I
+            {
+                znalezionoZwyciezce = true;
+            }
+            else if ((b1.Text == b2.Text) && (b2.Text == b3.Text) && b1.Text != "") //rząd II
+            {
+                znalezionoZwyciezce = true;
+            }
+            else if ((c1.Text == c2.Text) && (c2.Text == c3.Text) && c1.Text != "") //rząd III
+            {
+                znalezionoZwyciezce = true;
+            }
+            else if ((a1.Text == b1.Text) && (b1.Text == c1.Text) && a1.Text != "") //kolumna I
+            {
+                znalezionoZwyciezce = true;
+            }
+            else if ((a2.Text == b2.Text) && (b2.Text == c2.Text) && a2.Text != "") //kolumna II
+            {
+                znalezionoZwyciezce = true;
+            }
+            else if ((a3.Text == b3.Text) && (b3.Text == c3.Text) && a3.Text != "") //kolumna III
+            {
+                znalezionoZwyciezce = true;
+            }
+            else if ((a1.Text == b2.Text) && (b2.Text == c3.Text) && a1.Text != "") //skos I
+            {
+                znalezionoZwyciezce = true;
+            }
+            else if ((a3.Text == b2.Text) && (b2.Text == c1.Text) && a3.Text != "") //skos 2
+            {
+                znalezionoZwyciezce = true;
+            }
+
+            if (znalezionoZwyciezce == true)
+            {
+                string zwycięzca = "";
+                if (ruch)
+                {
+                    zwycięzca = "0";
+                }
+                else
+                {
+                    zwycięzca = "X";
+                }
+
+                MessageBox.Show("Zwycięzcą jest " + zwycięzca + "! Kliknij ok by wyczyścić planszę");
+                wyczysc();
+            }
+            else if (ruchIlosc == 9)
+            {
+                MessageBox.Show("Remis! " + "Kliknij ok by wyczyścić planszę");
+                wyczysc();
+            }
+        }
+        private void wyczysc()
+        {
+            a1.Text = "";
+            a1.Enabled = true;
+            a2.Text = "";
+            a2.Enabled = true;
+            a3.Text = "";
+            a3.Enabled = true;
+            b1.Text = "";
+            b1.Enabled = true;
+            b2.Text = "";
+            b2.Enabled = true;
+            b3.Text = "";
+            b3.Enabled = true;
+            c1.Text = "";
+            c1.Enabled = true;
+            c2.Text = "";
+            c2.Enabled = true;
+            c3.Text = "";
+            c3.Enabled = true;
+            ruchIlosc = 0;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            wyczysc();
+        }
+    }
+}
